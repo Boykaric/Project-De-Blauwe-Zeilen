36f9dc813e9f32ca78e56348f03b875e6c3f6835
KorteTV <hazardousUser@gmail.com>
2023-11-13 15:53:26 +0100

    banana

--- paginas/cursusdagen/cursusToevoegen.php
+++ paginas/cursusdagen/cursusToevoegen.php
@@ -68,27 +68,8 @@ if (isset($_POST['opslaan'])) {
     $instructeurId = $_POST['instructeur'];
     $actief = $_POST['actief'];
 
-<<<<<<< HEAD
-<?php
-if (isset($_POST['opslaan'])) {
-    // Haal de geselecteerde waarden uit het formulier
-    $cursusTijdId = $_POST['cursustijd'];
-    $bootId = $_POST['boot'];
-    $instructeurId = $_POST['instructeur'];
-    $actief = $_POST['actief'];
-
-    // Voeg de gegevens toe aan de 'planning' tabel
-    $sql = "INSERT INTO planning (cursus_id, boot_id, instructeur_id, actief) VALUES ('$cursusTijdId', '$bootId', '$instructeurId', '$actief')";
-    header("location: ?pagina=planningCursusDagen");
-    if (mysqli_query($conn, $sql)) {
-        echo "Gegevens zijn succesvol opgeslagen in de planning tabel.";
-    } else {
-        echo "Fout bij het opslaan van gegevens: " . mysqli_error($conn);
-    }
-=======
     // Voeg de gegevens toe aan de 'planning' tabel
     $result = $conn->query("INSERT INTO planning (cursus_id, boot_id, instructeur_id, actief) VALUES ('$cursusTijdId', '$bootId', '$instructeurId', '$actief')");
     header("location: ?pagina=planningCursusDagen");
->>>>>>> d7cdbfa08847aec7a3be7d6872444a596020307e
 }
 ?>
