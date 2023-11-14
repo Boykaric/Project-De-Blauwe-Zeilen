01267348cabd0c85099eb4bfef800136ce6ef40f
AbdullahRajab20001 <ar.abdullahrajab@gmail.com>
2023-11-14 10:55:55 +0100

    eh

--- home.php
+++ home.php
@@ -1,48 +1,7 @@
-<div id="carouselId" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-            <div class="row align-items-md-stretch">
-                <div class="header-box col-md-4">
-                    <div class="header-background h-100 p-5 text-white border rounded-3">
-                        <h2>Cursus vanaf 6 jaar</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis commodi aperiam quidem dolor nisi hic velit praesentium possimus nam autem! </p>
-                        <button class="header-btn btn btn-outline-primary" type="button">Example button</button>
-                    </div>
-                </div>
-            </div>
-            <img src="./assets/images/p1.jpg" class="header-img" alt="First slide">
-        </div>
-        <div class="carousel-item">
-            <div class="row align-items-md-stretch">
-                <div class="header-box col-md-4">
-                    <div class="header-background h-100 p-5 text-white border rounded-3">
-                        <h2>Cursus vanaf 6 jaar</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora temporibus voluptates, eaque obcaecati magni placeat similique dolorem ex eius! Labore quae libero adipisci rem minima sed eius, natus totam dolores et animi aspernatur fugiat doloribus possimus delectus, tempore ducimus.</p>
-                        <button class="header-btn btn btn-outline-primary" type="button">Example button</button>
-                    </div>
-                </div>
-            </div>
-            <img src="./assets/images/p2.jpg" class="header-img" alt="First slide">
-        </div>
-        <div class="carousel-item">
-            <div class="row align-items-md-stretch">
-                <div class="header-box col-md-4">
-                    <div class="header-background h-100 p-5 text-white border rounded-3">
-                        <h2>Cursus vanaf 6 jaar</h2>
-                        <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then,
-                            mix and match with additional component themes and more.</p>
-                        <button class="header-btn btn btn-outline-primary" type="button">Example button</button>
-                    </div>
-                </div>
-            </div>
-            <img src="./assets/images/p1.jpg" class="header-img" alt="First slide">
-        </div>
-    </div>
-
-</div>
-
-
-
+<?php
+require("./assets/onderdelen/header.php");
+?>
 <div class="container">
+bs5
 
 </div>
