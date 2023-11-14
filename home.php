a33680fd7fa2c69b2d90a4998fd584c22fd762c3
AbdullahRajab20001 <ar.abdullahrajab@gmail.com>
2023-11-14 10:13:53 +0100

    push

--- home.php
+++ home.php
@@ -1,27 +1,40 @@
 <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
-    <!-- <ol class="carousel-indicators">
-            <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
-            <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
-            <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Third slide"></li>
-        </ol> -->
     <div class="carousel-inner" role="listbox">
         <div class="carousel-item active">
             <div class="row align-items-md-stretch">
                 <div class="header-box col-md-4">
                     <div class="header-background h-100 p-5 text-white border rounded-3">
                         <h2>Cursus vanaf 6 jaar</h2>
-                        <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then,
-                            mix and match with additional component themes and more.</p>
-                        <button class="btn btn-outline-primary" type="button">Example button</button>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis commodi aperiam quidem dolor nisi hic velit praesentium possimus nam autem! </p>
+                        <button class="header-btn btn btn-outline-primary" type="button">Example button</button>
                     </div>
                 </div>
             </div>
-            <!-- <div class="header-box">
-                <h1 class="header-title">Cursus vanaf 6 jaar</h1>
-                <p class="header-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quos quae similique architecto possimus, porro ipsam labore dolorum ipsa incidunt.
-                </p>
-            </div> -->
+            <img src="./assets/images/p1.jpg" class="header-img" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <div class="row align-items-md-stretch">
+                <div class="header-box col-md-4">
+                    <div class="header-background h-100 p-5 text-white border rounded-3">
+                        <h2>Cursus vanaf 6 jaar</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora temporibus voluptates, eaque obcaecati magni placeat similique dolorem ex eius! Labore quae libero adipisci rem minima sed eius, natus totam dolores et animi aspernatur fugiat doloribus possimus delectus, tempore ducimus.</p>
+                        <button class="header-btn btn btn-outline-primary" type="button">Example button</button>
+                    </div>
+                </div>
+            </div>
+            <img src="./assets/images/p2.jpg" class="header-img" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <div class="row align-items-md-stretch">
+                <div class="header-box col-md-4">
+                    <div class="header-background h-100 p-5 text-white border rounded-3">
+                        <h2>Cursus vanaf 6 jaar</h2>
+                        <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then,
+                            mix and match with additional component themes and more.</p>
+                        <button class="header-btn btn btn-outline-primary" type="button">Example button</button>
+                    </div>
+                </div>
+            </div>
             <img src="./assets/images/p1.jpg" class="header-img" alt="First slide">
         </div>
     </div>
